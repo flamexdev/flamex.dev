0715baaafab22e3918d07947b5e6bbcd97b7d6f5
Flamex <acc@flamex.dev>
2022-01-05 15:29:41 +0100

    basic landing page concept

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,19 @@
 export default function Homepage() {
   return (
-    <div>
-      Hello World
+    <div className='h-screen flex justify-around items-center'>
+      <div className='flex items-center gap-10'>
+        <div className='font-bold'>
+          <p className='text-4xl'>
+            Flamex
+          </p>
+          <p className='text-gray-500'>
+            Consequat irure ut in ex qui adipisicing officia ullamco fugiat consequat anim occaecat amet.
+          </p>
+        </div>
+        <div className='flex justify-start w-1/2'>
+          <img src='https://media.flamex.dev/me_pfp.png' alt='Profile Picture' className='w-52 rounded-lg' />
+        </div>
+      </div>
     </div>
   )
 }
