0107c126f5e35d0252fd4f233dc31fa01b1bbd64
Flamex <acc@flamex.dev>
2022-01-06 18:30:10 +0100

    finished landing page

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,13 @@ import '../styles/tailwind.css'
 
 export default function render({ Component, pageProps }) {
   return (
-    <div className='w-screen min-h-screen bg-black text-white select-none'>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <div className='w-screen min-h-screen text-white select-none md:bg-gradient-to-tr bg-gradient-to-t from-black to-gray-800'>
+        <Component {...pageProps} />
+      </div>
+      <footer className='absolute bottom-0 left-0 w-screen bg-white bg-opacity-0 py-2 px-5 md:text-left text-center text-white text-opacity-25'>
+        &copy; 2022 - <a href="https://github.com/flamexdev/flamex.dev/blob/main/LICENSE" className='underline transition duration-100 ease-in-out hover:text-opacity-40 text-opacity-25 text-white'>Flamex</a>
+      </footer>
+    </>
   )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,46 @@
+const links = [
+  {
+    title: "Polywork",
+    url: "https://poly.work/flamex"
+  }, {
+    title: "Twitter",
+    url: "https://twitter.com/flamexdev"
+  }, {
+    title: "GitHub",
+    url: "https://github.com/flamexdev"
+  }
+]
+
 export default function Homepage() {
   return (
     <div className='h-screen flex justify-around items-center'>
-      <div className='flex items-center gap-10'>
-        <div className='font-bold'>
-          <p className='text-4xl'>
-            Flamex
-          </p>
-          <p className='text-gray-500'>
-            Consequat irure ut in ex qui adipisicing officia ullamco fugiat consequat anim occaecat amet.
-          </p>
+      <div className='p-4'>
+        <div className='flex items-center md:gap-10 gap-5 md:flex-row-reverse'>
+          <div className='flex justify-start'>
+            <img src='https://media.flamex.dev/me_pfp_upscaled.png' alt='Profile Picture' className='md:w-52 w-28 rounded-lg' />
+          </div>
+          <div className='font-bold md:w-min w-2/3'>
+            <p className='md:text-4xl text-3xl animate-bounce'>
+              Flamex
+            </p>
+            <p className='text-gray-500 font-mono md:whitespace-nowrap leading-4'>
+              Passionate self-taught software engineer from Germany
+            </p>
+            <p className='md:flex items-center gap-2 mt-5 hidden'>
+              {links.map(e => (
+                <a href={e.url} className='px-4 py-1 rounded-sm bg-indigo-500 transition duration-150 ease-in-out hover:bg-indigo-600'>
+                  {e.title}
+                </a>
+              ))}
+            </p>
+          </div>
         </div>
-        <div className='flex justify-start w-1/2'>
-          <img src='https://media.flamex.dev/me_pfp.png' alt='Profile Picture' className='w-52 rounded-lg' />
+        <div className='md:hidden flex items-center gap-2 mt-8 font-bold'>
+          {links.map(e => (
+            <a href={e.url} className='px-3 py-1 rounded-sm bg-indigo-500 transition duration-150 ease-in-out hover:bg-indigo-600 text-sm'>
+              {e.title}
+            </a>
+          ))}
         </div>
       </div>
     </div>
